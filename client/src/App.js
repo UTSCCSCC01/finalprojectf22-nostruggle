3b66089a531a30e9bcc0dca1939d59dd30108404
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-09-23 23:33:07 -0400

    Added post feature placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Container } from '@material-ui/core';
+import React, { useState, useRef } from 'react';
+import { Button, Container, Input } from '@material-ui/core';
 import './App.css';
 import axios from 'axios'
 
@@ -7,18 +7,25 @@ function App() {
 
   const [ users, setUsers ] = useState([])
   const [ error, setError ] = useState("")
+  const inputRef = useRef()
 
   const goToUsers = async () =>{
     axios.get('http://localhost:5000/users/get').then( res => setUsers(res.data)).catch( err => setError(err.message))
   }
-
+  const addUser = async () =>{
+    setError("Post from client coming soon")
+  }
   return (
     <Container maxWidth="lg">
       { error != "" ? error : null}
-      <Button  onClick={goToUsers} color="primary">Click to get users</Button>
+      <Button  onClick={goToUsers} color="primary">Click to get users (on /users/get )</Button>
       {users.map(user => 
         <h4>{user.username},{user.createdAt}</h4>
       )}
+      <label>Add a user</label>
+      <Input ref={inputRef}></Input>
+      <Button  onClick={addUser} color="primary">Click to add user ( on /users/post </Button>
+
     </Container>
   );
 }
