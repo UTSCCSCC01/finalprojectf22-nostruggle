873387d8a9ce0112bbe28e0c46ceaada7b29224d
j-tara <jorjani.tara@gmail.com>
2022-10-05 11:08:55 -0400

    Derivative calculator code supporting polynomials separated by "+"

--- client/src/derivativeCalculator.js
+++ client/src/derivativeCalculator.js
@@ -4,12 +4,65 @@ Expected user input so far: no spaces, no brackets, variables exclusively in ter
 Ex. 3x+7+2x^4 -> 3+8x^3
 */
 
-//Separating terms by + or - sign, and storing each term in an array
-//With a minus sign, first convert to plus sign and have negative coefficient, then store in array
+/*
+"3x+7+2x^4";
+"3x^2";
+"7";
+*/
+var eqn = "7";
+console.log(eqn);
+
+//Convert minus to plus sign with negative term, then store in array
+
+//Separating terms by + sign, and storing each term in an array
+const operators = [];
+for (const s of eqn) {
+    if (s == '+') {
+        operators.push(s);
+    }
+}
+
+const terms = eqn.split("+"); 
 
 //Deriving each term and store in string array
-//Case 1: Only number/constant -> disregard term entirely 
-//Case 2: constant * var (no exponent) -> only capture constant
-//Case 3: constant * var (with exponent) -> exponent*constant = new constant, var same, exponenet - 1 = new exponenet
+const derivedTerms = [];
+for (var i = 0; i < terms.length; i++) {
+    if (/^\d$/.test(terms[i])) {
+        derivedTerms.push("0");
+    }
+    else if (terms[i].includes('^')) {
+        var constant = parseInt(terms[i].substring(0, terms[i].indexOf("x")));
+        var exponent = parseInt(terms[i].substring(terms[i].indexOf("^") + 1,));
+        var newConstant = constant*exponent;
+        var newExponent = exponent - 1;
+
+        //Handling when new exponent becomes "^1" (removing it)
+        var derivedTerm; 
+        if (newExponent == 1) {
+            derivedTerm = newConstant + "x";
+        }
+        else {
+            derivedTerm = newConstant + "x^" + newExponent;
+        }
+
+        derivedTerms.push(derivedTerm);
+    }
+    else {
+        var constant = terms[i].substring(0, terms[i].indexOf("x"));
+        derivedTerms.push(constant);
+    }
+}
 
 //Output string array based on derived terms and prior operations to console
+var countOps = 0;
+var completeDerivative = '';
+for (var countDerived = 0; countDerived < derivedTerms.length; countDerived++) {
+    completeDerivative += derivedTerms[countDerived];
+    if (countOps < operators.length) {
+        completeDerivative += operators[countOps];
+        countOps++;
+    }
+}
+//Removes occurences of "+0" or "-0"
+completeDerivative = completeDerivative.replace(/(\+|-)0/g, '');
+console.log(completeDerivative);
