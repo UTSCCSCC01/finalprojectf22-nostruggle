391be067f2705e93df7c19c3f80609834ba10f27
MadisonMajarais <madison.majarais@gmail.com>
2022-10-06 00:14:57 -0400

    NOS-32 Added work that I did when following the tutorial
    Followed this tutorial: https://www.youtube.com/watch?v=7CqJlxBYj-M

---
+++ Tutorial/mern-exercise-tracker/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import EditExercise from "./components/edit-exercises.components";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
+
+function App() {
+  return (
+  <div className="App">
+      Hello World!
+    </div>
+  );
+
+   /* <Router>
+      <Navbar/>
+      <br/>
+      <Route path ="/" exact component={ExercisesList} />
+      <Route path = "//edit/:id" component = {EditExercise} />
+      <Route path="/create"component={CreateExercise}/>
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
+  */
+}
+    
+ 
+
+export default App;
---
+++ Tutorial/mern-exercise-tracker/src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUsers extends Component {
+    render(){
+        return(
+            <div>
+                <p>You are on the Create Users component! </p>
+            </div>
+        )
+    }
+}
