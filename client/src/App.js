4c176b4324cf0534d68e07ea0e67938f27f49ded
Zanesjk <zanesjk@gmail.com>
2022-10-06 18:49:33 -0400

    NOS-15 Implemented UI for redirecting to the calculators

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,16 @@ import React, { useState, useRef } from 'react';
 import { Button, Container, Input } from '@mui/material';
 import './App.css';
 import Features from './components/features'
+import { useNavigate } from "react-router-dom";
+import Zane from './components/Zane';
 function App() {
+
+  const [buttonPopup, setButtonPopup] = useState(false);
 
   // temporary navigator for setup, will be replaced with actual toggles/navbars
   const [ page, setPage ] = useState("")
+
+  
 
   return (
     <Container maxWidth="lg">
@@ -16,11 +22,14 @@ function App() {
       <Button onClick={() => setPage("Madison")}>Madison</Button>
       <Button onClick={() => setPage("Tara")}>Tara</Button>
       <Button onClick={() => setPage("Christine")}>Christine</Button>
-      <Button onClick={() => setPage("Zane")}>Zane</Button>
-
+      <Button onClick={() => setButtonPopup(true)}>Zane</Button>
+      <Zane trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <h3>Linear Algebra Calculators</h3>
+      </Zane>
       <Features page={page}/>
 
     </Container>
+
   );
 }
 
---
+++ client/src/components/Zane.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from '@mui/material';
+function Zane(props){
+    
+    const developing = () => {
+        alert("developing!");
+    }
+
+    return (props.trigger) ? (
+        <div className="AC">
+            <div className='AC-inner'>
+                
+                <button className="close-btn" onClick={developing}>Matrix Solver</button>
+                <br/>
+                <button className="cal" onClick={developing}>Linear Transformation Multiplier</button>
+                <br/>
+                <button className="cal" onClick={developing}>Eigenvalues and Eigenvectors Solver</button>
+                <br/>
+                <button className="cal" onClick={() => props.setTrigger(false)}>close</button>
+                { props.children }
+
+            </div>
+        </div>
+
+        
+    ) : "";
+
+
+    
+}
+
+export default Zane;
