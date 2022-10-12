0cad438cffd82779d050e7b92c83ba83366e2826
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-10-12 17:22:06 -0400

    NOS-15 set max time interval to be 1 day

--- client/src/components/features/Timer/constants.js
+++ client/src/components/features/Timer/constants.js
@@ -15,4 +15,4 @@ export const timerBreakInterval = [
     '90:00'
 ]
 
-export const timeFormat = "^(([0-9][0-9]:)?[0-5][0-9]:)?[0-5][0-9]$"
+export const timeFormat = "^((([0-1][0-9]|[2][0-3]):)?[0-5][0-9]:)?[0-5][0-9]$"
