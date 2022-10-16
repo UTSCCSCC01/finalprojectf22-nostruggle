a4d1243bf733931fb76c52fecf9dbf732d166174
MadisonMajarais <madison.majarais@gmail.com>
2022-10-15 21:20:24 -0400

    NOS-37 Update input validation and functionality of tags for new posts
    Added input validation and displaying error messages when creating a new post.  Added functionality for choosing a tag

--- client/src/features/CreatePost/CreatePost.js
+++ client/src/features/CreatePost/CreatePost.js
@@ -4,31 +4,63 @@ import axios from 'axios'
 
 function CreatePost(){
     
-    const [forumPost, setForumPost] = useState({username: "example", password: "content"});
-    const [title, setTitle] = useState({title: ''});
-    const [content, setContent] = useState({content: ''});
-    const [ activeTags, setActiveTags ] = useState([]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const [contentFilled, setContentFilled ] = useState(true);
+    const [titleFilled, setTitleFilled ] = useState(true);
+
+    const [tagSelected, setTagSelected] = useState('');
+
+    const [tagError, setTagError] = useState(false);
+
     const [postData, setPostData] = useState({ title: '',
         content: '',
         created_by: 'default-user',
-        tags: [],
+        tags: "",
         created_At: Date.now,
         nLikes: 0
     });
 
-    const handleClick = (event) => {
-        
+    const handleClick = (props) => {
+        console.log(props.tag);
+        setTagSelected(props.tag);
+        console.log(tagSelected);
+
+        setTagError(false);
+        setPostData(previousState => {return {...previousState, tags: props.tag}});
+
     }
 
     const createForumPost = async (event) => {
     
         event.preventDefault();
 
-        //setPostData(previousState => { return {...previousState, title: `${title}`, content: `${content}`}});
-        await axios.post('http://localhost:2800/forumPosts/post', postData)
+        await axios.post(process.env.REACT_APP_SERVER_URL + '/forumPosts/post', postData)
         .then(res => console.log(res.data))
         .catch(e => {
             console.log(e);
+   
+            if(title == ''){
+                setTitleFilled(false);
+                console.log("title is set to false")
+            }
+            else{
+                setTitleFilled(true);
+                console.log("title is set to true")
+            }
+            if(content == ''){
+                setContentFilled(false);
+            }
+            else{
+                setContentFilled(true);
+            }
+
+            if(tagSelected == ""){
+                setTagError(true);
+            }
+            console.log(title);
+            console.log(titleFilled);
             return <h3>Please add a title and content</h3>
         });
         
@@ -37,11 +69,13 @@ function CreatePost(){
     const enterTitle = (event) => {
         setTitle(event.target.value);
         console.log({title});
+        setTitleFilled(true);
         setPostData(previousState => { return {...previousState, title: event.target.value}});
     }
 
     const enterContent = (event) => {
         setContent(event.target.value);
+        setContentFilled(true);
         setPostData(previousState => { return {...previousState, content: event.target.value}});
     }
 
@@ -57,6 +91,8 @@ function CreatePost(){
                 label="Title of Post" 
                 variant="outlined"
                 fullWidth={true}
+                error={!titleFilled}
+                helperText={!titleFilled ? "Please enter a title" : ""}
                 onChange={enterTitle}
                 />
             </p>
@@ -68,14 +104,17 @@ function CreatePost(){
                 variant="outlined"
                 fullWidth={true}
                 multiline={true}
+                error={!contentFilled}
+                helperText={!contentFilled ? "Please enter content for the post" : ""}
                 rows={8}
                 onChange={enterContent}
                 /> 
             </p>
-
             <h3>Select tags:</h3>
 
-            <p>{ tags.map((tag) => <Chip label={ tag } onClick={ handleClick }/>)}</p>
+            <p>{ tags.map((tag) => <Chip label={ tag } color={tag == tagSelected ? "primary": "default"} onClick={ (e) => handleClick({tag}, e) }/>)}</p>
+
+            <p style={{color: "#d32f2f"}}>{tagError == true ? "Please select a tag to continue" : ""}</p>
 
             <Button onClick={ createForumPost }variant="contained">POST</Button>
             
