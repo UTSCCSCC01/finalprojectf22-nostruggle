89d278d5ffc34f00652b3985c85572917ae11cf6
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-10-20 20:25:22 -0400

    NOS-39 added functionality for selecting a user's task to track

--- client/src/features/Todo/StudyTimer/TimerSelectTask.jsx
+++ client/src/features/Todo/StudyTimer/TimerSelectTask.jsx
@@ -12,10 +12,8 @@ const TimerSelectTask = ({ onSelect, open, setOpen }) => {
     const fetchTasks = () => {
         ApiCall.get(`/tasks?userId=${userState.user._id}&done=false`)
         .then( res => {
-            console.log(res.data)
             let tasks = res.data
             tasks.sort((task1, task2) => {
-            
                 if (task1.deadline && task2.deadline) {
                     return Date.parse(task1.deadline) - Date.parse(task2.deadline)
                 } else if (task1.deadline) return -1
@@ -32,13 +30,10 @@ const TimerSelectTask = ({ onSelect, open, setOpen }) => {
         }
     }, [open])
 
-    useEffect(() => {
-        console.log(selectedTask)
-    }, [selectedTask])
-
     return (
         <Popover open={open} anchorOrigin={anchorPopover(300, 300)} anchor={document.body}>
             <Button onClick={() => setOpen(false)}>Back</Button>
+            <header><h2>Select Timer Task</h2></header>
             <Autocomplete
                 options={tasks}
                 getOptionLabel={(option) => option.title}
@@ -46,6 +41,7 @@ const TimerSelectTask = ({ onSelect, open, setOpen }) => {
                 onInputChange={(event, newInputValue) => {
                     setSelectedTask(tasks.find(task => newInputValue === task.title));
                 }}
+                size='small'
                 renderInput={(params) => <TextField {...params} label="Todo" />}
             />
             <Button onClick={() => onSelect(selectedTask)}>Set</Button>
