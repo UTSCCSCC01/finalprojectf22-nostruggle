20d9738f3bda9fa44597d73dbfe2384ef711e3f1
catherine-sun <mailcatherines@gmail.com>
2022-11-01 17:20:19 -0400

    NOS-16 Replaced  duplicated resize input method

--- client/src/components/forms/WrapContentField.jsx
+++ client/src/components/forms/WrapContentField.jsx
@@ -1,25 +1,6 @@
+import { resizeInput } from '../../features/Calculator/WrapContent'
 
 const WrapContentField = (props) => {
-
-    function getStringWidth(str, className) {
-        var strElement = document.createElement('span');
-
-        strElement.style.visibility = 'hidden';
-        strElement.style.padding = '4px';
-        strElement.className = className;
-        strElement.innerHTML = str;
-
-        document.body.appendChild(strElement);
-
-        return strElement.offsetWidth;
-    }
-
-    function resizeInput() {
-        const strlen = getStringWidth(props.value, props.type);
-        console.log('got ' + props.value.length + ' characters, trying ' + strlen + 'px');
-        return strlen !== 0 ? strlen + 'px' : 1;
-    }
-
 
     return (
         <input
