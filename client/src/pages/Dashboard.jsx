e5bf8485d006a70d410d5521b3e156de2611999b
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-11-02 20:23:51 -0400

    NOS-6 save dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,12 +1,9 @@
 import { Grid } from "@mui/material"
 
 import DashboardCard from "../components/dashboard/DashboardCard"
-import Scheduler from "../features/Todo/Scheduler"
 import { useState } from "react"
 import { dashboardItems } from "../features/Dashboard/constants"
 const Dashboard = () => {
-
-    const [ dashboardItems1, setDashboardItems ] = useState(['To-Do', 'Completed', 'Summary'])
 
     return (
         <div>
