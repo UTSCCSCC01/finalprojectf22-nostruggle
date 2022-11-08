44310ee4ec10d80468d2ebb658c11b42e4030639
j-tara <jorjani.tara@gmail.com>
2022-11-08 17:28:14 -0500

    NOS-16 Negative term addition bug fix

--- client/src/features/AlgebraCalculator/algebraCalculator.js
+++ client/src/features/AlgebraCalculator/algebraCalculator.js
@@ -4,48 +4,20 @@ function formatPrior(section) {
   return section;
 }
 
-/*
-function generateTerms(expression) {
-
-    expression = formatPrior(expression);
-    var terms = expression.split('+');
-    return terms;
-
-}
-*/
-
 function simplifyExpression(expression) {
 
     expression = formatPrior(expression);
-    //var terms = generateTerms(expression);
-
-    /*
-    var constant = 0;
-    for (var i = 0; i < terms.length; i++) {
-
-        //Constant case
-        if (!isNaN(terms[i])) {
-            constant += parseInt(terms[i]);
-        }
-
-        //x case (to do later)
-
-    }
-
-    var simplified = constant;
-    */
 
     for (var i = 0; i < expression.length; i++) {
       //Grouped together due to order of operations
-      console.log(expression[i]);
       if (expression[i] == '+' || (expression[i] == '-' && i > 0)) {
         if (expression[i] == '+') {
           var left;
           //If number on left side is positive
-          if (expression[i-1] != '-')
+          if (expression.substring(i - 2, i - 1) != '-')
             left = parseInt(expression.substring(i - 1, i));
           //If number on left side is negative 
-          else if (expression[i-1] == '-')
+          else if (expression.substring(i - 2, i - 1) == '-')
             left = parseInt(expression.substring(i - 2, i));
           var right = parseInt(expression.substring(i + 1, i + 2));
           console.log(left);
