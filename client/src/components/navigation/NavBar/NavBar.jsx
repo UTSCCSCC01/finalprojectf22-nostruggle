6f90f3dc9307ffa4fe3fff3de279769219868b25
catherine-sun <mailcatherines@gmail.com>
2022-11-15 04:36:23 -0500

    NOS-76 App bar shifts when opening drawer

--- client/src/components/navigation/NavBar/NavBar.jsx
+++ client/src/components/navigation/NavBar/NavBar.jsx
@@ -11,14 +11,16 @@ import './NavBar.css'
 import { navBarSignedInPages, navBarSignedOutPages } from '../../../pages/constants';
 import { useUserState } from '../../../features/SignUp/UserContext';
 import ToolsBar from '../ToolsBar'
+import { useEffect } from 'react';
 
 const NavBar = () => {
 
     const { userState } = useUserState();
     const navigate = useNavigate();
     const location = useLocation();
-    const [ openDrawer, setOpenDrawer ] = useState(false);
-    const [ openMenu, setOpenMenu ] = useState(false);
+    const [ openDrawer, toggleOpenDrawer ] = useState(false);
+    const [ openMenu, toggleOpenMenu ] = useState(false);
+    const [ offset, setOffset ] = useState(0);
     
     const showPage = (page) => {
         const buttonProps = {
@@ -44,22 +46,27 @@ const NavBar = () => {
             </Tooltip>
         )
     };
-    
+
+    useEffect(() => {
+        const element = document.getElementById('SideBar'); 
+        setOffset(element === null ? '0px' : element.offsetWidth + 'px');
+    }, [openDrawer])
+
     return (
         <>
             { openDrawer && userState.signedIn &&
-            <Drawer className='ToolbarDrawer' variant='persistent' anchor='left' open={ openDrawer } sx={{ width: 70, flexShrink: 0 }}>
-                <div className='SideBar'><ToolsBar/></div>
+            <Drawer className='ToolbarDrawer' variant='persistent' anchor='left' open={ openDrawer } sx={{ width: 70, flexShrink: 0 }} >
+                <div id='SideBar'><ToolsBar/></div>
             </Drawer>
             }
 
-            <AppBar sx={{ zIndex: 2000}} position='fixed'>
+            <AppBar sx={{ zIndex: 2000, left: offset, width: 'calc(100% - ' + offset + ')' }} position='fixed'>
                 <span>
                     <Toolbar className='Toolbar'>
                         {
                             userState.signedIn &&
                             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <IconButton onClick={ () => setOpenDrawer(!openDrawer) }>
+                                <IconButton onClick={ () => toggleOpenDrawer(!openDrawer) }>
                                     <HomeRepairService sx={{ fontSize: '40px', color: openDrawer === true ?  '#EFF3F6' : '' }} />
                                 </IconButton>
                             </Box>
@@ -70,7 +77,7 @@ const NavBar = () => {
 
                         <Box className='icon'>
                             <Tooltip title='Click to Open Menu'>
-                                <IconButton onClick={ () => setOpenMenu(!openMenu) }>
+                                <IconButton onClick={ () => toggleOpenMenu(!openMenu) }>
                                     <Avatar /*alt='put alt text' src='../../assets/images/???'*//>
                                 </IconButton>
                             </Tooltip>
