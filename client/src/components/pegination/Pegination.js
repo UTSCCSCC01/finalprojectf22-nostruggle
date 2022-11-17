9a10d38128763c1e3cc9b5a2ce18b737bd47e051
Zanesjk <zanesjk@gmail.com>
2022-11-16 21:30:52 -0500

    NOS-57 created Pegination API

---
+++ client/src/components/pegination/Pegination.js
@@ -0,0 +1,11 @@
+import { Pagination } from '@mui/material';
+
+function Pegination() {
+    return(
+        <div>
+            <Pagination count={10} color="primary" />
+        </div>
+    )
+}
+
+export default Pegination;
