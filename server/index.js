aaf492a932384a0cc9ea45293c7df9a31b46b8e1
Penguinfami <christine.spyphone.zhao@gmail.com>
2022-11-22 10:34:28 -0500

    NOS-69 added cors headers

--- server/index.js
+++ server/index.js
@@ -21,6 +21,13 @@ import notificationRouter from './routes/notifications.js';
 import answerRouter from './routes/answers.js';
 
 import commentRouter from './routes/comment.js';
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/users', userRouter);
 app.use('/forumPosts', postRouter);
 app.use('/openapi', openApiRouter);
